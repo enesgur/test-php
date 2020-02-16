869b2525c5adbd33a6318c0839e727331f4f1e2f
Enes Gür <enesaligur@gmail.com>
2020-02-16 17:06:40 +0300

    change code

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('memory_limit',-1);
 
 $i = 0;
 $a = [];
-while($i <= 10000000) {
+while($i <= 100) {
     $a[] = $i;
     $a[] = json_encode($a);
     $i++;
 }
-
